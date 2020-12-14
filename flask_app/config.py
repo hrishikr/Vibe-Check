8400f0dd4aeb3a1bce3f27f183a89106786dd660
Arjun Kothakota <arjunreddykothakota@gmail.com>
2020-12-14 14:15:14 -0500

    please

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,3 +1,5 @@
 # Stores all configuration values
 SECRET_KEY = b'\n\xb5\x8a\x96\xaa.+2~\xc3\x0e(?\xb0\x17\xd9'
 MONGODB_URI = "mongodb://localhost:27017/my_database"
+SPOTIFY_CLIENT_ID = "38c3150e3d6c47dfbcfb372321adc4e7"
+SPOTIFY_CLIENT_SECRET = "4b2cb5cee0e04184ac100b97bda5efec"
